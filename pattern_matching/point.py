c601c79aa36f756cdf726c5bb8919e95f4f0488f
Minjun Kim (Lapis0875) <lapis0875@kakao.com>
2021-02-17 16:40:30 +0000

    feat: Update pattern_matching/point.py
    Finish example for now.

--- pattern_matching/point.py
+++ pattern_matching/point.py
@@ -1,14 +1,29 @@
 from dataclasses import dataclass
+from math import sqrt
+from typing import Tuple, Union
 
 @dataclass
 class Point2D:
-    x: int
-    y: int
+    x: float
+    y: float
 
 @dataclass
 class Point3d:
-    x: int
-    y: int
-    z: int
+    x: float
+    y: float
+    z: float
 
 
+Point = Union[Point2D, Point3d, Tuple[float, float], Tuple[float, float, float]]
+
+
+def get_distance(a: Point, b: Point) -> float:
+    match a, b:
+        case a:= Point3D(), b:= Point3D():
+            return sqrt((b.x-a.x)**2 + (b.y-a.y)**2 + (b.z-a.z)**2)
+        case a:= Point2D(), b:= Point2D():
+            return sqrt((b.x-a.x)**2 + (b.y-a.y)**2)
+        case tuple(ax, ay), tuple(bx, by):
+            return sqrt((bx-ax)**2 + (by-ay)**2)
+        case tuple(ax, ay, az), tuple(bx, by, bz):
+            return sqrt((bx-ax)**2 + (by-ay)**2 + (bz-az)**2)
